6e804b8c8519e7cf7b6004c1a2f344dd574dc350
johnmccambridge7 <john.l.macdonald.22@dartmouth.edu>
2020-01-22 23:53:49 -0500

    more changes

--- index.js
+++ index.js
@@ -7,7 +7,18 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 
 app.post('/new', (req, res) => {
-    console.log('Got body:', req.body);
+    const featureData = req.body.feature;
+    const coordinateData = req.body.geometry;
+
+    const cleridData = [featureData.Clerids_PerDay1, 
+                        featureData.Clerids_PerDay2, 
+                        featureData.Clerids_PerDay3, 
+                        featureData.Clerids_PerDay4, 
+                        featureData.Clerids_PerDay5, 
+                        featureData.Clerids_PerDay6];
+
+    console.log(cleridData);
+
     res.sendStatus(200);
 });
 

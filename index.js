b458e2cc2619d705dc64c87b8f10e2bb09a0da54
johnmccambridge7 <john.l.macdonald.22@dartmouth.edu>
2020-01-22 23:21:43 -0500

    changing port number

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
 
-const port = 8000;
+const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
+app.get('/test', (req, res) => {
     res.send('Hello Earth!');
 });
 

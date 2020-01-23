b49555fdd59bd7943b51e1bb4fcb31e01cf4d3ab
johnmccambridge7 <john.l.macdonald.22@dartmouth.edu>
2020-01-23 01:50:50 -0500

    more changes

--- src/controllers/AutomationController.js
+++ src/controllers/AutomationController.js
@@ -2,6 +2,8 @@ const uploadSurvey = async (information) => {
     const url = 'mongodb://heroku_cdlx19v1:72dnr3u2vdaju3r3irnvtci3ge@ds023485.mlab.com:23485/heroku_cdlx19v1'//process.env.MONGODB_URI;
     const MongoClient = require('mongodb').MongoClient;
 
+    console.log(process.env.MONGODB_URI);
+
     MongoClient.connect(url, (error, db) => {
         if(error) throw error;
         

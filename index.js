333b93bf87ee7eb5f1e392a0c1d8bb48fbce008a
johnmccambridge7 <john.l.macdonald.22@dartmouth.edu>
2020-01-22 23:43:47 -0500

    changes to the middleware

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 const port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/new', (req, res) => {
     console.log('Got body:', req.body);

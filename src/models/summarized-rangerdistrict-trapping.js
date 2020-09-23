e4b2551e1e454371845ab3aa19a4486b7199221d
jeffzyliu <jeffzyliu@gmail.com>
2020-09-23 14:46:39 -0400

    create first draft of 134 indexing

--- src/models/summarized-rangerdistrict-trapping.js
+++ src/models/summarized-rangerdistrict-trapping.js
@@ -1,13 +1,21 @@
 import mongoose, { Schema } from 'mongoose';
 
 // collection 4: rangerdistrict-level trapping data aggregated by year
+// multiple indexing on state/county/year for index intersection
+// speeds up merging w/ spot data, also speeds up client-side reading
 const SummarizedRangerDistrictTrappingSchema = new Schema({
-  state: { type: String },
-  rangerDistrict: { type: String },
+  state: {
+    type: String,
+    index: true,
+  },
+  rangerDistrict: {
+    type: String,
+    index: true,
+  },
   year: {
     type: Number,
     min: 1900,
-    index: true, // speeds up join w/ spots by year
+    index: true,
   },
   spbCount: {
     type: Number,
--- src/models/unsummarized-trapping.js
+++ src/models/unsummarized-trapping.js
@@ -1,14 +1,25 @@
 import mongoose, { Schema } from 'mongoose';
 
 // collection 1: historical unsummarized trapping
+// multiple indexing on S/C/RD/Y to support aggregating individual years/states
+// consider removing C/RD indexes later because we probably don't need them
 const UnsummarizedTrappingSchema = new Schema({
-  state: { type: String },
-  county: { type: String },
-  rangerDistrict: { type: String },
+  state: {
+    type: String,
+    index: true,
+  },
+  county: {
+    type: String,
+    index: true,
+  },
+  rangerDistrict: {
+    type: String,
+    index: true,
+  },
   year: {
     type: Number,
     min: 1900,
-    index: true, // speeds up aggregation by year
+    index: true,
   },
   month: {
     type: Number,

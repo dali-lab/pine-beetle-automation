88bfac61d099dbfcd270f934b09f8fd205738bc9
Jeff Liu <jeffzyliu@gmail.com>
2020-10-03 02:14:13 -0400

    feat: updated unsummarized model, added json to map between csv fields and our fields

--- src/models/unsummarized-trapping.js
+++ src/models/unsummarized-trapping.js
@@ -3,12 +3,30 @@ import mongoose, { Schema } from 'mongoose';
 // collection 1: historical unsummarized trapping
 // multiple indexing on S/Y to support aggregating individual years/states
 const UnsummarizedTrappingSchema = new Schema({
+  bloom: {
+    type: String,
+  },
+  bloomDate: {
+    type: Date,
+  },
   cleridCount: {
     min: 0,
     type: Number,
   },
+  collectionDate: {
+    type: Date,
+  },
   county: {
     type: String,
+  },
+  daysActive: {
+    type: String,
+  },
+  endobrev: {
+    type: Number, // unsure of this
+  },
+  fips: {
+    type: Number,
   },
   latitude: {
     max: 90,
@@ -20,20 +38,30 @@ const UnsummarizedTrappingSchema = new Schema({
     min: -180,
     type: Number,
   },
-  month: {
-    max: 12,
-    min: 1,
-    type: Number,
+  lure: {
+    type: String,
   },
   rangerDistrict: {
     type: String,
+  },
+  season: {
+    type: String,
+  },
+  sirexLure: {
+    type: String, // unsure of this
   },
   spbCount: {
     min: 0,
     type: Number,
   },
+  startDate: {
+    type: Date,
+  },
   state: {
     index: true,
+    type: String,
+  },
+  trap: {
     type: String,
   },
   week: {

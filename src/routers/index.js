dd30fb7f0f61b8740808783c108b434821133b0e
Angela Zhang <angela.zhang@dali.dartmouth.edu>
2020-10-06 10:11:02 -0700

    spot data router

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,12 +1,14 @@
 import healthcheck from './healthcheck';
 import rModel from './r-model';
 import unsummarizedTrappingRouter from './unsummarized-trapping';
+import spotDataRouter from './spot-data';
 import summarizedCountyTrappingRouter from './summarized-county-trapping';
 import summarizedRangerDistrictTrappingRouter from './summarized-rangerdistrict-trapping';
 
 export default {
   healthcheck,
   'r-model': rModel,
+  'spot-data': spotDataRouter,
   'summarized-county-trapping': summarizedCountyTrappingRouter,
   'summarized-rangerdistrict-trapping': summarizedRangerDistrictTrappingRouter,
   'unsummarized-trapping': unsummarizedTrappingRouter,

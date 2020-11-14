be7b34cb53db4e8d0420edecf22930ae1aa1a66c
Thomas Monfre <tmonfre1@gmail.com>
2020-11-14 14:52:23 -0500

    feat: pipeline for new survey123 input

--- src/controllers/survey123.js
+++ src/controllers/survey123.js
@@ -24,6 +24,26 @@ import {
   survey123WebhookUnpackCreator,
 } from '../utils';
 
+import {
+  summarizeStateYear as countySummarizeStateYear,
+} from './summarized-county-trapping';
+
+import {
+  summarizeStateYear as rangerDistrictSummarizeStateYear,
+} from './summarized-rangerdistrict-trapping';
+
+import {
+  mergeSpots,
+} from './spot-data';
+
+import {
+  generateStateYearPredictions as countyGenerateStateYearPredictions,
+} from './county-prediction';
+
+import {
+  generateStateYearPredictions as rangerDistrictGenerateStateYearPredictions,
+} from './rd-prediction';
+
 const unsummarizedModelAttributes = getModelAttributes(UnsummarizedTrappingModel);
 
 // ensures all attributes present
@@ -54,7 +74,50 @@ export const uploadCsv = csvUploadSurvey123Creator(
   stateToAbbrevTransform,
 );
 
+/**
+ * runs pipeline by summarizing data, appending spot data, and generating predictions
+ * @param {String} state state to aggregate and predict on
+ * @param {String} year year to aggregate and predict on
+ * @returns {Promise<Object>} result data of each bulk operation
+ */
+export const runPipeline = async (state, rawYear = '0') => {
+  const year = parseInt(rawYear, 10);
+
+  // summarize county and ranger district data
+  const summarizeResult = await Promise.all([
+    countySummarizeStateYear(state, year),
+    rangerDistrictSummarizeStateYear(state, year),
+  ]);
+
+  // append spot data
+  const spotAppendResult = await Promise.all([
+    mergeSpots('t2', 'county', year - 2, state),
+    mergeSpots('t1', 'county', year - 1, state),
+    mergeSpots('t2', 'rangerDistrict', year - 2, state),
+    mergeSpots('t1', 'rangerDistrict', year - 1, state),
+  ]);
+
+  // generate predictions
+  const predictionResult = await Promise.all([
+    countyGenerateStateYearPredictions(state, year),
+    rangerDistrictGenerateStateYearPredictions(state, year),
+  ]);
+
+  return {
+    predictionResult,
+    spotAppendResult,
+    summarizeResult,
+  };
+};
+
+/**
+ * upload survey123 data to unsummarized collection -- should be called by webhook
+ * @param {Object} rawData raw data from survey123
+ * @returns {Promise<Array>} delete result and insert result data
+ */
 export const uploadSurvey123FromWebhook = async (rawData) => {
+  const isFinalCollection = rawData.Is_Final_Collection === 'yes';
+
   const data = survey123WebhookUnpacker(rawData)
     .map(stateToAbbrevTransform);
 
@@ -62,6 +125,12 @@ export const uploadSurvey123FromWebhook = async (rawData) => {
 
   const deleteRes = await UnsummarizedTrappingModel.bulkWrite([deleteOp], { ordered: false });
   const insertRes = await UnsummarizedTrappingModel.bulkWrite(insertOp, { ordered: false });
+
+  // run pipeline if final collection
+  if (isFinalCollection) {
+    const { state, year } = data.find((d) => !!d);
+    if (state && year) await runPipeline(state, year);
+  }
 
   return [deleteRes, insertRes];
 };

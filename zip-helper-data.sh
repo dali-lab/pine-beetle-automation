4d21e47d9b9335b7a20ba34fea489a9d0c935943
Thomas Monfre <tmonfre1@gmail.com>
2020-11-21 14:39:06 -0500

    feat: zip helper data during build

---
+++ zip-helper-data.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+rm -rf src/constants/helper-data.zip
+zip src/constants/helper-data.zip src/constants/abbrev-to-state.json src/constants/csv-to-spots-county.json src/constants/csv-to-spots-rangerdistrict.json src/constants/csv-to-unsummarized.json src/constants/nf-rd-mapping.json src/constants/rd-name-mapping.json

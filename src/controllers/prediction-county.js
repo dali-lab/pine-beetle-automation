942dc69a72b0c05b0aeefd3b8064460b50913b92
gracewanggw <grace.wang.24@dartmouth.edu>
2022-02-16 18:28:44 -0500

    removed unused file

--- src/controllers/prediction-county.js
+++
@@ -1,8 +0,0 @@
-import * as rModel from './r-model';
-
-import {
-  SummarizedCountyModel,
-  UnsummarizedTrappingModel,
-} from '../models';
-
-import { RESPONSE_TYPES, COLLECTION_NAMES } from '../constants';

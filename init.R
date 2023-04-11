7f89f32ab90d5ba586b6d89a0f43232319adc39e
Jeff Liu <jeffzyliu@gmail.com>
2023-04-10 20:08:11 -0700

    fix: use R cache

--- init.R
+++ init.R
@@ -1,1 +1,5 @@
-install.packages("jsonlite", dependencies = TRUE)
+# install.packages("jsonlite", dependencies = TRUE)
+
+# this is a helper function from https://github.com/virtualstaticvoid/heroku-buildpack-r#r-package-installation-helper
+# that uses the cache to not do reinstalls
+helpers.installPackages("jsonlite")

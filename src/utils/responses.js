40ab8f5f5dc4ec21bb18891193b001d5f92030cc
Ewelina Sarad-Cichuta <evelinasarad@gmail.com>
2026-03-09 19:37:01 +0100

    Clean code

--- src/utils/responses.js
+++ src/utils/responses.js
@@ -16,7 +16,7 @@ export const generateResponse = (responseType, payload) => {
   return {
     status,
     type,
-    ...(status >= 200 && status < 300 ? { data: payload } : { error: payload }),
+    ...(status === RESPONSE_CODES.SUCCESS.status ? { data: payload } : { error: payload }),
   };
 };
 
